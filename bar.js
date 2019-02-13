5e4adc9ceea76ad6be4f0ee0e9480d384481dc79
datou <1392767180@qq.com>
2019-02-13 21:29:42 +0800

    add pictureShow

---
+++ bar.js
@@ -0,0 +1,11 @@
+let option = document.getElementsByClassName("option")[0];
+let barInner = document.getElementsByClassName("bar-inner")[0];
+option.addEventListener('click', function (e) {
+  e = e || window.event;
+  let target = e.target || e.srcElement;
+  if (target.tagName === 'SPAN') {
+    let index = [].indexOf.call(option.children,target) + 1;
+    barInner.classList = 'bar-inner';
+    barInner.classList.add(`barStatus${index}`);
+  }
+}, false);
