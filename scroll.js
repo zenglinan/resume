79d2ebbe2533bf4e216d1150baa20422879ef1b3
datou <1392767180@qq.com>
2019-03-24 16:36:53 +0800

    优化导航栏滚动代码，添加过渡效果

--- scroll.js
+++ scroll.js
@@ -1,26 +1,5 @@
-let headerTop = document.getElementsByTagName('header')[0];
-
 window.onscroll = function () {
-  (getScrollOffset().y !== 0) ?
-  (handleBar(true)) :
-  (handleBar(false));
-}
-function getScrollOffset(){
-  if(window.pageXOffset){
-      return {
-          x : window.pageXOffset,
-          y : window.pageYOffset
-      }
-  }
-  else{
-      return {
-          x : document.body.scrollLeft + document.documentElement.scrollLeft,
-          y : document.body.scrollTop + document.documentElement.scrollTop
-      }
-  }
-}
-function handleBar (show) {
-  (show === true) ?
-  headerTop.classList.add('whiteBar') :
-  headerTop.classList.remove('whiteBar');
+  (window.scrollY !== 0) ?
+  (topHeader.classList.add('whiteBar')) :
+  (topHeader.classList.remove('whiteBar'));
 }
