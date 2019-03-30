65d1e3335c30c5ade60e0dd6560c71d4ae0f8334
datou <1392767180@qq.com>
2019-03-31 00:08:34 +0800

    增加滚动到指定位置时，导航栏下滑线依旧存在的效果

--- srcollTo.js
+++ srcollTo.js
@@ -1,15 +1,18 @@
 // ==========点击导航栏跳至相应部分==========
 let a = document.getElementsByClassName('navA');
 for (let i = 0; i < a.length; i++) {
-  a[i].addEventListener('click', function(e){
+  a[i].addEventListener('click', function (e) {
     e = e || window.event;
     e.preventDefault();
     let href = a[i].getAttribute('href');
     let targetEle = document.querySelector(href);
-    console.log(targetEle)
     window.scrollTo({
       top: targetEle.getBoundingClientRect().top + window.scrollY - topHeader.offsetHeight,
       behavior: 'smooth',
     })
+    for (let j = 0; j < a.length; j++) {
+      a[j].classList.remove('active');
+    }
+    a[i].classList.add('active');
   })
 }
