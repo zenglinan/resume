5c15b95c52191847a99ff546aa082d6b0a13d2d3
datou <1392767180@qq.com>
2019-04-21 01:23:08 +0800

    增加作品标题

---
+++ bar.js
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+=======
+// // ==========作品集的bar==========
+// let option = document.getElementsByClassName("option")[0];
+// let barInner = document.getElementsByClassName("bar-inner")[0];
+// option.addEventListener('click', function (e) {
+//   e = e || window.event;
+//   let target = e.target || e.srcElement;
+//   if (target.tagName === 'SPAN') {
+//     let index = [].indexOf.call(option.children,target) + 1;
+//     barInner.classList = 'bar-inner';
+//     barInner.classList.add(`barStatus${index}`);
+//   }
+// }, false);
+>>>>>>> 225187843724e33e78bdf036a8d956bab201c53f
