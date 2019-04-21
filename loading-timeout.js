ea6aaf9a7a546ab90adbc02892cd06f1986dc2e9
datou <1392767180@qq.com>
2019-04-21 12:58:04 +0800

    模块化js文件

---
+++ loading-timeout.js
@@ -0,0 +1,5 @@
+setTimeout(function () {
+  loading.classList.add('hide');
+  siteMain.classList.remove('heightNone');
+  siteMain.classList.add('active');
+}, 500)
--- scroll.js
+++ scroll.js
@@ -1,24 +1,26 @@
 // ==========滚动时导航栏效果==========
-
-let aNav = document.getElementsByClassName('navA');
-let aTarget = document.querySelectorAll("[atarget]");
-setTimeout(function(){
-  aTarget[0].classList.remove('offset');
-},600);
-aTarget[0].classList.add('slideUp');
-window.onscroll = function () {
-  (window.scrollY !== 0) ?
-    (topHeader.classList.add('whiteBar')) :
-    (topHeader.classList.remove('whiteBar'));
-  let index = 0;
-  for (let i = 0; i < aTarget.length; i++) {
-    if (Math.abs(aTarget[i].getBoundingClientRect().top) < Math.abs(aTarget[index].getBoundingClientRect().top)){
-      index = i;
+!function () {
+  let aNav = document.getElementsByClassName('navA');
+  let aTarget = document.querySelectorAll("[atarget]");
+  setTimeout(function () {
+    aTarget[0].classList.remove('offset');
+  }, 600);
+  aTarget[0].classList.add('slideUp');
+  window.onscroll = function () {
+    (window.scrollY !== 0) ?
+      (topHeader.classList.add('whiteBar')) :
+      (topHeader.classList.remove('whiteBar'));
+    let index = 0;
+    for (let i = 0; i < aTarget.length; i++) {
+      if (Math.abs(aTarget[i].getBoundingClientRect().top) < Math.abs(aTarget[index].getBoundingClientRect().top)) {
+        index = i;
+      }
     }
+    for (let j = 0; j < aTarget.length; j++) {
+      aNav[j].classList.remove("active");
+    }
+    aNav[index].classList.add("active");
+    aTarget[index].classList.remove('offset');
   }
-  for(let j = 0; j < aTarget.length; j ++){
-    aNav[j].classList.remove("active");
-  }
-  aNav[index].classList.add("active");
-  aTarget[index].classList.remove('offset');
-}
+
+}()
